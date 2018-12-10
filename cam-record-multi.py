3b8a09ecc4c8401dae15d1747678f45473f28878
manuj2n <manu.j2n@gmail.com>
2018-12-10 13:06:12 +0000

    evolution

--- cam-record-multi.py
+++ cam-record-multi.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# capture des fichiers videos successifs
 import picamera
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (640, 480)
-    camera.start_recording('1.h264')
-    camera.wait_recording(5)
-    for i in range(2, 11):
-        camera.split_recording('%d.h264' % i)
-        camera.wait_recording(5)
+    camera.resolution = (1280, 720)
+    camera.start_recording('/home/pi/capture/1.h264')
+    camera.wait_recording(120)
+    for i in range(2, 6):
+        camera.split_recording('/home/pi/capture/%d.h264' % i)
+        camera.wait_recording(120)
     camera.stop_recording()
