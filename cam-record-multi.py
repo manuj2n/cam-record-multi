64ed81ccb2f2da73c9b425eea5bc0421794f04e2
manuj2n <manu.j2n@gmail.com>
2018-12-11 10:26:40 +0000

    3 fichiers distincts 5min

--- cam-record-multi.py
+++ cam-record-multi.py
@@ -7,13 +7,15 @@ import datetime
 
 with picamera.PiCamera() as camera:
 	camera.resolution = (1280, 720)
-	hC = datetime.datetime.now()
-	fichier = '{:%H%M%S}'.format(hC) 
-	camera.start_recording('/home/pi/capture/%s.h264' % fichier)
-	camera.wait_recording(300)
-	for i in range(1, 3):
+#	hC = datetime.datetime.now()
+#	fichier = '{:%H%M%S}'.format(hC) 
+#	camera.start_recording('/home/pi/capture/%s.h264' % fichier)
+#	camera.wait_recording(300)
+	for i in range(1, 4):
 		hC = datetime.datetime.now()
 		fichier = '{:%H%M%S}'.format(hC)
-		camera.split_recording('/home/pi/capture/%s.h264' % fichier)
+		fichier = fichier + '-' + str(i)
+		camera.start_recording('/home/pi/capture/%s.h264' % fichier)
+		#camera.split_recording('/home/pi/capture/%s.h264' % fichier)
 		camera.wait_recording(300)
-	camera.stop_recording()
+		camera.stop_recording()
